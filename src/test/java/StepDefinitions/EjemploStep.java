ed67faf3be129365c547ea2109cafbaf21f08f9b
institutoweb <137358271+institutoweb@users.noreply.github.com>
2024-07-09 20:40:45 -0300

    Update EjemploStep.java

--- src/test/java/StepDefinitions/EjemploStep.java
+++ src/test/java/StepDefinitions/EjemploStep.java
@@ -2,12 +2,14 @@ package StepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.testng.Assert;
 
 public class EjemploStep {
 
     @Given("que ingrese en saucedemo.com")
     public void IngresoSaucedemo(){
         System.out.println("Ingresé en Saucedemo.com");
+        Assert.fail("falló la prueba");
     }
 
     @Then("ingreso un usuario en la pantalla de login")
